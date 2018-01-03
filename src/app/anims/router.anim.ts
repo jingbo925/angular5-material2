e8459dcbdb699ed0c800e5d033fcdcce2ccf54d1
gy104 <wangjingbo@intgg.com>
2018-01-03 18:57:22 +0800

    tes

--- src/app/anims/router.anim.ts
+++ src/app/anims/router.anim.ts
@@ -1,17 +1,23 @@
 /**
  * Created by gy104 on 2017/12/27.
  */
-import { trigger, state, transition, style, animate, keyframes} from "@angular/animations";
+import { trigger, state, transition, style, animate, keyframes, group} from "@angular/animations";
 export const sildeToRight = trigger('routerAnim', [
   state('void', style({'position':'fixed', 'width': '100%', 'height':'80%'})),
   state('*', style({'position':'fixed', 'width': '100%', 'height':'80%'})),
   transition('void => *', [
-    style({transform: 'translateX(-100%)'}),
-    animate('500ms ease-in-out', style({transform: 'translateX(0)'}))
+    style({transform: 'translateX(-100%)',opacity:0}),
+    group([
+      animate('500ms ease-in-out', style({transform: 'translateX(0)'})),
+      animate('300ms ease-in', style({opacity:1}))
+    ])
   ]),
   transition('* => void', [
-    style({transform: 'translateX(0)'}),
-    animate('500ms ease-in-out', style({transform: 'translateX(100%)'}))
+    style({transform: 'translateX(0)',opacity: 1}),
+    group([
+      animate('500ms ease-in-out', style({transform: 'translateX(100%)'})),
+      animate('300ms ease-in', style({opacity:0}))
+    ])
   ]),
 ]);
 
--- src/app/project/project-list/project-list.component.ts
+++ src/app/project/project-list/project-list.component.ts
@@ -4,12 +4,14 @@ import {NewProjectComponent} from "../new-project/new-project.component";
 import {InviteComponent} from "../invite/invite.component";
 import {ConfirmDialogComponent} from "../../share/confirm-dialog/confirm-dialog.component";
 import { sildeToRight} from '../../anims/router.anim'
+import {listAnimation} from "../../anims/list.anim";
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss'],
   animations: [
-    sildeToRight
+    sildeToRight,
+    listAnimation
   ]
 })
 export class ProjectListComponent implements OnInit {
@@ -17,11 +19,13 @@ export class ProjectListComponent implements OnInit {
   @HostBinding('@routerAnim') state;
   projects = [
     {
+      "id":1,
       "name":"企业协作平台",
       "desc": "这是一个企业内部项目",
       "coverImg":"assets/img/covers/0.jpg"
     },
     {
+      "id":2,
       "name":"企业协作平台",
       "desc": "这是一个企业内部项目",
       "coverImg":"assets/img/covers/1.jpg"
@@ -30,7 +34,20 @@ export class ProjectListComponent implements OnInit {
   constructor(private dialog: MatDialog) { }
 
   openNewProjectDialog() {
-    this.dialog.open(NewProjectComponent,{data: {title: '新增项目'}});
+    let dialogRef = this.dialog.open(NewProjectComponent,{data: {title: '新增项目'}});
+
+    dialogRef.afterClosed()
+      .subscribe(
+        (result) => {
+          this.projects = [ ... this.projects,{
+            "id":3,
+            "name":"企业协作平台",
+            "desc": "这是一个新项目",
+            "coverImg":"assets/img/covers/8.jpg"
+          } ]
+        }
+      )
+
   }
 
   launchInviteDialog(){
@@ -41,12 +58,13 @@ export class ProjectListComponent implements OnInit {
     this.dialog.open(NewProjectComponent, {data: {title: "编辑项目"}})
   }
 
-  launchDelDialog() {
+  launchDelDialog(project) {
     let dialogRef = this.dialog.open(ConfirmDialogComponent,{data: {title: "删除项目", content: "您确定删除该项目吗？"}})
     dialogRef.afterClosed()
       .subscribe(
         (result) => {
           console.log(result);
+          this.projects = this.projects.filter( p => p.id != project.id)
         }
       )
   }
